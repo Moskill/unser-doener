053125c5aae263c185b2d06872cc18125d75b046
unknown <cborek@web.de>
2021-12-02 14:12:45 +0100

    ReactAdmin part for the Orders created

--- src/components/adminArea/index.js
+++ src/components/adminArea/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
+import dataProvider from '../../dataProvider';
 import MenuList from './Lists/MenuList';
 import UserList from './Lists/UserList';
 import SideDishesList from './Lists/SideDishesList';
 import MenuSideDishesList from './Lists/MenuSideDishesList';
+import OrderList from './Lists/OrderList';
 import MenuCreate from './Creators/MenuCreate';
-import SideDishCreate from './Creators/SideDishCreate'
-import dataProvider from '../../dataProvider';
+import SideDishCreate from './Creators/SideDishCreate';
+import OrderCreate from './Creators/OrderCreate';
 
 function AdminArea() {
 
@@ -17,6 +19,7 @@ function AdminArea() {
         <Resource name='users' list={UserList} />
         <Resource name='sideDishes' list={SideDishesList} create={SideDishCreate} />
         <Resource name='menuSideDishes' list={MenuSideDishesList} />
+        <Resource name='orders' list={OrderList} create={OrderCreate} />
       </Admin>
     </>
   )
