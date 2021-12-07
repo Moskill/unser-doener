6bd40626152d0e9369c60034db41de17ff755eeb
unknown <cborek@web.de>
2021-12-07 07:54:10 +0100

    SideDish sekection func on CartItem added

--- src/components/userArea/Sidebar/CartItem.js
+++ src/components/userArea/Sidebar/CartItem.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './CartItem.css';
 
 function CartItem({data, index}) {
+  const standardSideDishes = ["TsaTsiki", "Krautsalat-weiss", "Krautsalat-rot", "Bauernsalat"];
   const [deleteItem, setDeleteItem] = useState();
   const [sideDishes, setSideDishes] = useState();
+  const [selectedSideDishes, setSelectedSideDishes] = useState({[index]: standardSideDishes});
 
+
+  // Zu löschende Items in den State schmeißen
   useEffect(() => {
     setDeleteItem(JSON.parse( localStorage.getItem('myCart')))
   }, [])
   
   let tmpCart = deleteItem;
   
+  // Menus aus dem Cart löschen
   const removeItemFromCart = (e) => {
     e.preventDefault();
     tmpCart.myCart.splice(e.target.value, 1);
@@ -19,12 +24,23 @@ function CartItem({data, index}) {
     window.location.reload();
   }
 
+  // Einmal alles SideDishes holen
   useEffect(() => {
     fetch("http://localhost:8080/sideDishes")
     .then((response) => response.json())
     .then((data) => setSideDishes(data));
   }, []);
 
+  // Die SideDishes sammeln, die ausgewählt wurden
+  const sideDishesSelector = (sideDish) => {
+    let tempArr = selectedSideDishes[index];
+    tempArr.includes(sideDish) ?
+    tempArr.splice(tempArr.indexOf(sideDish), 1) : 
+    tempArr.push(sideDish); 
+    setSelectedSideDishes({[index]: tempArr});
+  }
+
+  // console.log(selectedSideDishes && selectedSideDishes[index])
 
   return (
     <>
@@ -50,13 +66,49 @@ function CartItem({data, index}) {
           </div>  
           {sideDishes && (
             <>
-              <input type="checkbox" value={sideDishes[3].name} /><span>{sideDishes[3].name}</span><br/>
-              <input type="checkbox" value={sideDishes[2].name} /><span>{sideDishes[2].name}</span><br/>
-              <input type="checkbox" value={sideDishes[0].name} /><span>{sideDishes[0].name}</span><br/>
-              <p className="item-dressing">Soßen</p>
-              <input type="checkbox" value={sideDishes[4].name} /><span>{sideDishes[4].name}</span><br/>
-              <input type="checkbox" value={sideDishes[5].name} /><span>{sideDishes[5].name}</span><br/>
-              <input type="checkbox" value={sideDishes[6].name} /><span>{sideDishes[6].name}</span><br/>
+              <span className="side-dishes" className="side-dishes" style={
+                selectedSideDishes[index].includes(sideDishes[3].name) ? 
+                {backgroundColor: 'limegreen'} : 
+                {backgroundColor: 'red', textDecoration: 'line-through'}}
+                onClick={() => sideDishesSelector(sideDishes[3].name)}>{sideDishes[3].name}
+              </span><br/>
+
+              <span className="side-dishes" style={
+                selectedSideDishes[index].includes(sideDishes[2].name) ? 
+                {backgroundColor: 'limegreen'} : 
+                {backgroundColor: 'red', textDecoration: 'line-through'}}
+                onClick={() => sideDishesSelector(sideDishes[2].name)}>{sideDishes[2].name}
+              </span><br/>
+
+              <span className="side-dishes" style={
+                selectedSideDishes[index].includes(sideDishes[0].name) ? 
+                {backgroundColor: 'limegreen'} : 
+                {backgroundColor: 'red', textDecoration: 'line-through'}}
+                onClick={() => sideDishesSelector(sideDishes[0].name)}>{sideDishes[0].name}
+              </span><br/>
+
+              <span className="side-dishes" style={
+                selectedSideDishes[index].includes(sideDishes[4].name) ? 
+                {backgroundColor: 'limegreen'} : 
+                {backgroundColor: 'red', textDecoration: 'line-through'}}
+                onClick={() => sideDishesSelector(sideDishes[4].name)}>{sideDishes[4].name}
+              </span><br/>
+
+              <span className="side-dishes" style={
+                selectedSideDishes[index].includes(sideDishes[5].name) ? 
+                {backgroundColor: 'limegreen'} : 
+                {backgroundColor: 'red', textDecoration: 'line-through'}}
+                onClick={() => sideDishesSelector(sideDishes[5].name)}>{sideDishes[5].name}
+              </span><br/>
+
+              <span className="side-dishes" style={
+                selectedSideDishes[index].includes(sideDishes[6].name) ? 
+                {backgroundColor: 'limegreen'} : 
+                {backgroundColor: 'red', textDecoration: 'line-through'}}
+                onClick={() => sideDishesSelector(sideDishes[6].name)}>{sideDishes[6].name}
+              </span><br/>
+
+              <input type="hidden" value={selectedSideDishes} />
             </>
           )}  
           </div>
